ad5a57acf29b55ffef2d5bd68e18e2f27f1facf6
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2024-05-07 16:40:22 +0200

    yarn development mode: Rewrites to api calls on 8080

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   transpilePackages: [
     'antd', 
@@ -20,9 +19,27 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  basePath: '/boba',
   assetPrefix: '/boba',
-  images: { unoptimized: true }
+  images: { unoptimized: true } // necessary for output = 'export'
+}
+
+if(process.env.NODE_ENV === 'development') {
+  // forward API calls to Python backend
+  nextConfig.rewrites = () => {
+    return [
+        {
+          source: "/api/:path*",
+          destination: "http://localhost:8080/api/:path*",
+        },
+      ];
+  };
+} else {
+  // build a static site, without server-side features
+  // only for production build, otherwise rewrites for dev won't work
+  nextConfig.output = 'export';
+  // production build mounts assets under /boba
+  nextConfig.basePath = '/boba';
+  
 }
 
 module.exports = nextConfig
