7639d89a01fa67e738a4fc896b035b84de5802ff
Héctor Tortosa <hector.tortosa@thoughtworks.com>
2024-05-10 10:59:39 +0300

    style: Apply formatter used in CI

--- app/api/models/explore_request.py
+++ app/api/models/explore_request.py
@@ -1,5 +1,7 @@
+# © 2024 Thoughtworks, Inc. | Thoughtworks Pre-Existing Intellectual Property | See License file for permissions.
 from typing import Optional
 from pydantic import BaseModel
+
 
 class ExploreRequest(BaseModel):
     input: str
