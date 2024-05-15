d899f090e34c47ff7e6420ae35ae6cfba8944b12
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2024-05-15 09:07:28 +0200

    Remove Helmet from scenarios, not imported there

--- ui/src/pages/scenarios.js
+++ ui/src/pages/scenarios.js
@@ -414,17 +414,6 @@ const Home = () => {
             visible={displayMode == "plot"}
           />
         </div>
-
-        <div>
-          <Helmet>
-            <script
-              type="module"
-              src="https://gradio.s3-us-west-2.amazonaws.com/4.31.0/gradio.js"
-            ></script>
-          </Helmet>
-
-          <GradioApp></GradioApp>
-        </div>
       </div>
     </>
   );
