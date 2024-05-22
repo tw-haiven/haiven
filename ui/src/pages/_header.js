95089e81627d591103d7cc4f0466861123399f68
Héctor Tortosa <hector.tortosa@thoughtworks.com>
2024-05-22 13:46:48 +0300

    feat: Add new heder to Nextjs app

--- ui/src/pages/_header.js
+++ ui/src/pages/_header.js
@@ -4,26 +4,24 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="page-header">
-      <Menu mode="horizontal" className="header">
-        <div className="header-title">
-          <h1>Haiven</h1>
-          <h2>Team assistant</h2>
+      <header class="header">
+        <div class="left-section">
+          <div class="logo">
+            <img src="/boba/thoughtworks_logo.png" alt="Logo" />
+          </div>
+          <div class="separator"></div>
+          <div class="title">Haiven team assistant</div>
         </div>
-        <div className="header-logo">
-          <a href="https://www.thoughtworks.com">
-            {/* 400, 65 */}
-            {/* 280, 45 */}
-            <Image
-              alt="Thoughtworks"
-              id="tw-logo"
-              src="/boba/thoughtworks_logo.png"
-              title="Thoughtworks"
-              width={160}
-              height={26}
-            />
-          </a>
+        <div class="mode-switch">
+          <span>Choose your working mode</span>
+          <button class="mode-button" id="standardMode">
+            Guided mode
+          </button>
+          <button class="mode-button" id="advancedMode">
+            Chat mode
+          </button>
         </div>
-      </Menu>
+      </header>
     </div>
   );
 }
