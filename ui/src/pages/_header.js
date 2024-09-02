ed5daedcb15086bd082db3d2927640094c5a3dc9
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2024-09-02 15:31:59 +0200

    feat: Add about & knowledge to top header

--- ui/src/pages/_header.js
+++ ui/src/pages/_header.js
@@ -1,6 +1,6 @@
 // © 2024 Thoughtworks, Inc. | Licensed under the Apache License, Version 2.0  | See LICENSE.md file for permissions.
-import { Menu } from "antd";
-import Image from "next/image";
+import Link from "next/link";
+import { Button } from "antd";
 
 export default function Header() {
   return (
@@ -13,13 +13,13 @@ export default function Header() {
           <div className="separator"></div>
           <div className="title">Haiven team assistant</div>
         </div>
-        <div className="mode-switch">
-          <button className="mode-button mode-selected mode-left">
-            Guided mode
-          </button>
-          <button className="mode-button mode-other mode-right">
-            <a href="/analysis">Chat mode</a>
-          </button>
+        <div className="header-links">
+          <span type="link">
+            <Link href="/about">About</Link>
+          </span>
+          <span type="link">
+            <Link href="/knowledge">Knowledge Overview</Link>
+          </span>
         </div>
       </header>
     </div>
