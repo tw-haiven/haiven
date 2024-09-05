80c95242285b4441e1352e08f108c04eee8388b4
SSK-14 <sanjaykumar1481999@gmail.com>
2024-09-05 17:02:05 +0530

    Remove the gradio ui

--- app/app.py
+++ app/app.py
@@ -1,18 +1,12 @@
 # © 2024 Thoughtworks, Inc. | Licensed under the Apache License, Version 2.0  | See LICENSE.md file for permissions.
-import gradio as gr
 from api.boba_api import BobaApi
 from knowledge_manager import KnowledgeManager
 from llms.chats import ChatManager, ServerChatSessionMemory
 from llms.image_description_service import ImageDescriptionService
 from llms.clients import ChatClientFactory
 from llms.model import Model
-from logger import HaivenLogger
 from prompts.prompts_factory import PromptsFactory
 from server import Server
-from ui.event_handler import EventHandler
-from ui.navigation import NavigationManager
-from ui.ui import UIBaseComponents
-from ui.ui_factory import UIFactory
 from config_service import ConfigService
 
 
@@ -37,17 +31,6 @@ class App:
 
         image_service = self.create_image_service(config_service)
 
-        self.ui_factory = UIFactory(
-            ui_base_components=UIBaseComponents(config_service),
-            prompts_factory=prompts_factory,
-            navigation_manager=NavigationManager(),
-            event_handler=EventHandler(HaivenLogger),
-            prompts_parent_dir=knowledge_pack_path,
-            knowledge_manager=knowledge_manager,
-            chat_manager=chat_manager,
-            image_service=self.create_image_service(config_service),
-        )
-
         self.server = Server(
             chat_manager,
             config_service,
@@ -61,22 +44,4 @@ class App:
         ).create()
 
     def launch_via_fastapi_wrapper(self):
-        if self.ui_factory is not None:
-            ui_components = [
-                ("create_ui_analysts", "get_analysis_path"),
-                ("create_ui_testing", "get_testing_path"),
-                ("create_ui_coding", "get_coding_path"),
-                ("create_ui_knowledge", "get_knowledge_path"),
-                ("create_ui_about", "get_about_path"),
-                ("create_plain_chat", "get_chat_path"),
-            ]
-
-            for create_ui, get_path in ui_components:
-                ui_component = getattr(self.ui_factory, create_ui)()
-                path = getattr(self.ui_factory.navigation_manager, get_path)()
-
-                gr.mount_gradio_app(
-                    self.server, ui_component, path=path, root_path=path
-                )
-
         return self.server
