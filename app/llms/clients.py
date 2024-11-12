82c55646c91ff6c818c2335ec7649439e605fe63
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2024-11-12 12:06:33 +0100

    Add a long summary to the mock
    Typical use case of testing the UI layout

--- app/llms/clients.py
+++ app/llms/clients.py
@@ -67,8 +67,8 @@ class MockModelClient:
         ]
         if "json" in message.lower():
             full_test_scenario = {
-                "title": "Full mocked scenario",
-                "summary": "A description",
+                "title": "Full mocked scenario with a very long title title title title",
+                "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                 "probability": "Low - Most users would have no reason to deny",
                 "impact": "Medium - This could lead to accountability issues",
             }
