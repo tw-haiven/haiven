79b69ab6279a69b7b572e63420ed585caddcad97
Shobana M <shobanam@thoughtworks.com>
2025-02-05 12:17:27 +0530

    Upgrade frontend packages. Upgrade tailwindcss from v3 to v4

--- ui/postcss.config.js
+++ ui/postcss.config.js
@@ -1,7 +1,6 @@
 // © 2024 Thoughtworks, Inc. | Licensed under the Apache License, Version 2.0  | See LICENSE.md file for permissions.
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
