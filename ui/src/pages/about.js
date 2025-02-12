4a791f11067bb6592cceaf472ccb5c0224e26f02
Shobana M <shobanam@thoughtworks.com>
2025-02-12 17:52:56 +0530

    Remove data-processing tab in ABout page

--- ui/src/pages/about.js
+++ ui/src/pages/about.js
@@ -54,62 +54,6 @@ const AboutPage = ({}) => {
     </div>
   );
 
-  const dataProcessingText = (
-    <div>
-      <h3>3rd party model services</h3>
-      <p>
-        Please be conscious of this and responsible about what data you enter
-        when you're having a chat conversation.
-      </p>
-      <p>
-        Each chat message is shared with an AI model. Depending on which "AI
-        service and model" you had opted, that's where your chat messages go
-        (typically either a cloud service, or a model running on your local
-        machine).
-      </p>
-      <p>
-        Most of the 3rd party model services have terms & conditions that say
-        that they do NOT use your data to fine-tune their models in the future.
-        However, these services do typically persist chat conversations, at
-        least temporarily, so your data is stored on their servers, at least
-        temporarily.
-      </p>
-      <p>
-        Therefore, please comply with your organization's data privacy and
-        security policies when using this tool. In particular, you should never
-        add any PII (personally identifiable information) as part of your
-        instructions to the AI. For all other types of data, consider the
-        sensitivity and confidentiality in the context of your particular
-        situation, and consult your organization's data privacy policies.
-      </p>
-      <h3>Data Collection</h3>
-      <p>
-        The only data that gets persisted by this application itself is in the
-        form of logs.
-      </p>
-      <p>The application logs data about the following events:</p>
-      <ul>
-        <li>Whenever a page is loaded, to track amount of activity</li>
-        <li>
-          Whenever a chat session is being started, to track amount of activity
-        </li>
-        <li>
-          How many times a certain prompt is used, to track popularity of a
-          prompt
-        </li>
-        <li>
-          Clicks on thumbs up and thumbs down, to track how useful the tool is
-          for users
-        </li>
-      </ul>
-      <p>
-        User IDs from the OAuth session are included in each log entry to get an
-        idea of how many different users are using the application.
-      </p>
-      <p>The application does NOT persist the contents of the chat sessions.</p>
-    </div>
-  );
-
   const tabs = [
     {
       key: "guidelines",
@@ -126,11 +70,6 @@ const AboutPage = ({}) => {
       key: "background",
       label: "What is Haiven?",
       children: <div>{aboutText}</div>,
-    },
-    {
-      key: "data-processing",
-      label: "Data processing",
-      children: <div>{dataProcessingText}</div>,
     },
   ];
 
