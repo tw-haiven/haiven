5c2ec895e987fd0003817afb3967082dd2ebdd71
Shobana M <shobanam@thoughtworks.com>
2025-02-18 11:32:15 +0530

    Add context tooltip if context is empty

--- ui/src/app/_context_choice.js
+++ ui/src/app/_context_choice.js
@@ -3,11 +3,15 @@ import { Select } from "antd";
 import HelpTooltip from "./_help_tooltip";
 
 function ContextChoice({ contexts, value, onChange }) {
+  const tooltipMessage =
+    contexts.length === 1
+      ? "There are no contexts configured in the knowledge pack. Contexts can provide project-specific information about domain and architecture that Haiven can reuse across prompts."
+      : "Choose a context from your knowledge pack that is relevant to the domain, architecture, or team you are working on.";
   return (
     <div className="user-input">
       <label>
         Add your context
-        <HelpTooltip text="Choose a context from your knowledge pack that is relevant to the domain, architecture, or team you are working on." />
+        <HelpTooltip text={tooltipMessage} />
       </label>
       <Select
         onChange={onChange}
