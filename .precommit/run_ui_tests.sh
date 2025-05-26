4f976866f057fefdab1e3dd64bd7e5559810cb84
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2025-05-26 09:40:04 +0200

    fix: Run UI tests in GH Actions, outside of pre-commit
    Our pre-commit was skipping UI tests when on CI, so we need to resurrect it as a separate step. (Running it as part of the pre-commit led to node setup errors, it gives us more control to have the tests running in a separate action)

--- .precommit/run_ui_tests.sh
+++ .precommit/run_ui_tests.sh
@@ -2,7 +2,7 @@
 
 # Check if the script is running inside GitHub Actions
 if [ "$CI" = "true" ]; then
-  echo "Skipping UI tests in CI environment..."
+  echo "Skipping pre-commit UI tests in CI environment, they will run in their dedicated Action"
   exit 0
 else
   echo "Running UI tests locally..."
